7e1bea0414ecd804d58ad7b2c1e9d75517eb35da
phdljr <phdljr@naver.com>
2023-02-17 01:14:13 +0900

    init:chatting programming

---
+++ src/main/java/kr/ac/project/chatingsocket/ChatingSocketApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.project.chatingsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatingSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatingSocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/project/chatingsocket/ChatingSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.project.chatingsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatingSocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
