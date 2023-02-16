f8a42ec46c591327c413554c181f6bb4c8fbcdd0
phdljr <phdljr@naver.com>
2023-02-17 02:24:02 +0900

    feat: add SocketHandler

---
+++ src/main/java/kr/ac/project/chatingsocket/config/SocketTextHandler.java
@@ -0,0 +1,35 @@
+package kr.ac.project.chatingsocket.config;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SocketTextHandler extends TextWebSocketHandler {
+
+    private final Set<WebSocketSession> sessions = ConcurrentHashMap.newKeySet();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+        System.out.println("누군가 접속함: " + sessions);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        System.out.println(message);
+        for (WebSocketSession s : sessions) {
+            s.sendMessage(new TextMessage("Hi " + payload + "!"));
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+        System.out.println("누군가 떠나감: " + sessions);
+    }
+}
---
+++ src/main/java/kr/ac/project/chatingsocket/config/WebSocketConfig.java
@@ -0,0 +1,17 @@
+package kr.ac.project.chatingsocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new SocketTextHandler(), "/ws/join")
+                .setAllowedOrigins("http://localhost:3000")
+                .withSockJS();
+    }
+}
